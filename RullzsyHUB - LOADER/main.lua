33c2e6fa360cd4767d1d28d9664dc68e2f5ec404
0xtunggereung@gmail.com <rullzsyhub@gmail.com>
2025-10-25 21:26:38 +0700

    first commit

--- RullzsyHUB - LOADER/main.lua
+++ RullzsyHUB - LOADER/main.lua
@@ -89,10 +89,10 @@ ScriptTab:CreateButton({
 
 -- Script 9
 ScriptTab:CreateButton({
-    Name = "🟢 MOUNT KAWAH EXPEDITION (PRO)",
+    Name = "🔴 MOUNT KAWAH EXPEDITION (PRO) MAINTENANCE",
     Callback = function()
         Rayfield:Notify({Title="Executing", Image="file", Content="Loading MOUNT KAWAH EXPEDITION PRO...", Duration=4})
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/0x0x0x0xblaze/scripts/refs/heads/main/RullzsyHUB%20-%20MOUNT%20KAWAH%20TERJUN%20PRO/main.lua"))()
+        -- loadstring(game:HttpGet("https://raw.githubusercontent.com/0x0x0x0xblaze/scripts/refs/heads/main/RullzsyHUB%20-%20MOUNT%20KAWAH%20TERJUN%20PRO/main.lua"))()
     end
 })
 
